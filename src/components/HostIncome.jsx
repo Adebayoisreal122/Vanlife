e737105cbf6a30c70ac8d9a727f585f888c1c5b6
adebayo isreal <adebayoisrealight122@gmail.com>
2024-02-18 19:13:44 +0100

    working on the host

--- src/components/HostIncome.jsx
+++ src/components/HostIncome.jsx
@@ -2,31 +2,35 @@ import React, { useEffect, useRef } from 'react';
 import Chart from 'chart.js/auto';
 
 const HostIncome = () => {
-  const chartRef = useRef(null); // Create a ref to hold the canvas element
+  // const chartRef = useRef(null); // Create a ref to hold the canvas element
 
-  useEffect(() => {
-    const ctx = chartRef.current.getContext('2d'); // Get the 2D context of the canvas element
+  // useEffect(() => {
+  //   const ctx = chartRef.current.getContext('2d'); 
 
-    const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-    const yValues = [55, 49, 44, 24, 15];
-    const barColors = ["red", "green","blue","orange","brown"];
+  //   const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+  //   const yValues = [55, 49, 44, 24, 15];
+  //   const barColors = ["red", "green","blue","orange","brown"];
 
-    new Chart(ctx, { // Pass ctx (2D context) instead of "myChart"
-      type: "bar",
-      data: {
-        labels: xValues,
-        datasets: [{
-          backgroundColor: barColors,
-          data: yValues
-        }]
-      },
-      options: {} // You can customize options here if needed
-    });
-  }, []);
+  //   new Chart(ctx, {
+  //     type: "bar",
+  //     data: {
+  //       labels: xValues,
+  //       datasets: [{
+  //         backgroundColor: barColors,
+  //         data: yValues
+  //       }]
+  //     },
+  //     options: {} // You can customize options here if needed
+  //   });
+  // }, []);
 
   return (
     <>
-      <canvas ref={chartRef} id="myChart"></canvas> {/* Assign ref to the canvas element */}
+      {/* <canvas ref={chartRef} id="myChart"  width={200} height={200} ></canvas> Assign ref to the canvas element */}
+
+
+
+      <h1>lets go</h1>
     </>
   );
 };
