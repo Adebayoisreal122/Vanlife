55c44fc38f92a5db29c15c1770e349eaac6359df
adebayo isreal <adebayoisrealight122@gmail.com>
2024-02-26 00:39:24 +0100

    i intall formik for the sign up

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,19 +1,59 @@
 import React from 'react'
+import { useFormik } from "formik"
+import * as yup from "yup"
 
 const Signin = () => {
+
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+
+  const validationSchema = yup.object({
+    email: yup.string().email("enter valid email address"),
+    password: yup.string().matches(passwordRegex, "The password must be at least 8 character, and should include one uppercase, one lowercase" ),
+  });
+
+
+  
+  const formik = useFormik({
+    initialValues: {
+        email: '',
+        password: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: values => {
+      console.log(values)
+    },
+  });
+
+
   return (
     <>
 
-<div className='container signal '>
+<div className=' container-fluid signal '>
 <center>
 
 <h1>
   Sign in to your account
 </h1>
-<form className='form'>
-<input type="email" name="" placeholder='Email address' id="" />
-<input type="password" name="" placeholder='Password' id="" />
-</form>
+
+
+<form  onSubmit={formik.handleSubmit} className="form">
+            <label>
+                <input name="email " required=""  placeholder="" type="email"
+                 onChange={formik.handleChange}
+                className="input  "/>
+                <span>Email Address</span>
+            </label> 
+            <span className="text-danger">{formik.errors.email}</span>
+            <label>
+                <input name="password" required="" placeholder="" type="password"
+                 onChange={formik.handleChange}
+                className="input "/>
+                <span>Password</span>
+            </label>
+            <span className="text-danger">{formik.errors.password}</span>
+            <button type="submit"  className="submit">Submit</button>
+        </form>
+
 </center>
 </div>
 
